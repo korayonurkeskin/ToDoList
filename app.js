a41c9c4a659d79ff6d675b146eb5e8766ed78b40
KorayKeskin <100540170+korayonurkeskin@users.noreply.github.com>
2023-11-08 23:07:49 -0800

    Added mark as complete functionality.

--- app.js
+++ app.js
@@ -7,12 +7,21 @@ for (let i = 0; i < taskList.length; i++) {
     span.className = 'close';
     span.appendChild(txt);
     taskList[i].appendChild(span);
-}
+};
 
 // add delete task functionality
 const closeButtons = document.getElementsByClassName('close');
 for (let btn of closeButtons) {
     btn.onclick = function() {
         this.parentElement.style.display = "none";
-    }
-}
+    };
+};
+
+// mark as complete functionality
+let list = document.querySelector('ul')
+list.addEventListener('click', function(e) {
+    console.log(e.target.classList);
+    if (e.target.tagName === 'LI') {
+        e.target.classList.toggle('checked')
+    } 
+}, false);
